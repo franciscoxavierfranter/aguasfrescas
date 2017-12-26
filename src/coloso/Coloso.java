5e1fb273b47d49274ed7f7701298cc8a3071ee0b
panchox <>
2017-12-26 19:10:48 -0300

    inicio soloso FQuila

---
+++ src/coloso/Coloso.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coloso;
+
+/**
+ *
+ * @author Duoc UC
+ */
+public class Coloso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
